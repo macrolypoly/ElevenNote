6b3c8141db463877bcc0812a29512ff410f0ed89
macrolypoly <krodriguez61@ivytech.edu>
2021-04-22 18:13:15 -0400

    potato

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Content { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUTC { get; set; }
+
+        [Display(Name = "Category Name")]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
