702db5a2bc848c69e2f129be0df7a4ba190bf4bc
macrolypoly <krodriguez61@ivytech.edu>
2021-04-22 19:02:15 -0400

    awaw

---
+++ ElevenNOte.Data/Migrations/202104222252486_K.cs
@@ -0,0 +1,31 @@
+namespace ElevenNOte.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class K : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Category",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            AddColumn("dbo.Note", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Note", "CategoryId");
+            AddForeignKey("dbo.Note", "CategoryId", "dbo.Category", "CategoryId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Note", "CategoryId", "dbo.Category");
+            DropIndex("dbo.Note", new[] { "CategoryId" });
+            DropColumn("dbo.Note", "CategoryId");
+            DropTable("dbo.Category");
+        }
+    }
+}
